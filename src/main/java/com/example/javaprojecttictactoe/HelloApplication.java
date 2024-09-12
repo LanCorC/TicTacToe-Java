f207e7cda4cdc70b9f5780acf156cd3327ee14d8
LanCorC <lanc3.corcega@gmail.com>
2024-09-12 18:40:26 +1200

    An attempt to encapsulate gameGridVisual, or the GridPane for the BorderPane's Center.

--- src/main/java/com/example/javaprojecttictactoe/HelloApplication.java
+++ src/main/java/com/example/javaprojecttictactoe/HelloApplication.java
@@ -31,70 +31,12 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene mainScene = new Scene(fxmlLoader.load(), 320, 240);
 
         //Set parent
         BorderPane root = new BorderPane();
 
         //Set center
-        GridPane gameGridVisual = new GridPane();
-        gameGridVisual.setHgap(5);
-        gameGridVisual.setVgap(5);
-
-        gameGridVisual.setPrefSize(stage.getWidth(), stage.getHeight());
-
-        gameGridVisual.setAlignment(Pos.CENTER);
-//        gameGridVisual.setPadding(new Insets(15, 15, 15, 15));
-        //TODO: temporary. i want lines inside the grids, between grids, not a 'border'
-//        gameGridVisual.setBorder(new Border(new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.FULL)));
-
-//        gameGridVisual.;
-        Node[][] gameGrid = new Text[3][3];
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-//                Text text = new Text((i + j)% 2 == 0 ? "X" : "O");
-//                text.setTextAlignment(TextAlignment.CENTER);
-//                gameGrid[i][j] = text;
-//                text.setStyle("-fx-background-color: aqua");
-//                gameGridVisual.add(text, i, j);
-//                text.setFont(new Font(15));
-
-                Button button = new Button(Game.getEmptySymbol());
-//                button.setText((i + j)% 2 == 0 ? "X" : "O");
-//                button.fontProperty().bind();
-//                button.setAlignment(Pos.CENTER);
-                button.widthProperty().addListener((observableValue, oldValue, newValue) -> button.setFont(new Font(newValue.doubleValue()/3.0)));
-                button.setStyle("-fx-background-color: %s; -fx-background-radius: 0".formatted("WHITESMOKE"));
-//                button.setBackground(Background.EMPTY);
-
-                GridPane.setConstraints(button, i, j, 1, 1, HPos.CENTER, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
-//                button.maxWidthProperty().bind(gameGridVisual.widthProperty().divide(5));
-//                button.maxHeightProperty().bind(gameGridVisual.heightProperty().divide(5));
-                button.maxWidthProperty().bind(gameGridVisual.widthProperty());
-                button.maxHeightProperty().bind(gameGridVisual.heightProperty());
-                button.setOnAction((x)->{
-                    Game.play(new Pair<>(GridPane.getRowIndex(button), GridPane.getColumnIndex(button)));
-                });
-                gameGridVisual.add(button, i, j);
-            }
-        }
-
-        //TODO: do not include in final, debugging
-//        gameGridVisual.setGridLinesVisible(true);
-
-        //Add constraints to all columns
-        ColumnConstraints cc = new ColumnConstraints();
-        cc.setPercentWidth(33);
-        RowConstraints rc = new RowConstraints();
-        rc.setPercentHeight(33);
-        for(int i = 0; i < 3; i++) {
-            gameGridVisual.getColumnConstraints().add(cc);
-            gameGridVisual.getRowConstraints().add(rc);
-        }
-
-
+        GridPane gameGridVisual = configureGrid();
 
         //Set status messages
         //e.g. "Player's turn" or "X wins!"
@@ -140,22 +82,13 @@ public class HelloApplication extends Application {
         BorderPane.setMargin(gameGridVisual, new Insets(15, 15, 15, 15));
         root.setCenter(vb);
 
-//        settings.setPadding(new Insets(50,50,50,50));
-//        HBox.setMargin(settings, new Insets(5, 0, 0, 5));
         topBar.setSpacing(5);
         HBox.setHgrow(settings, Priority.ALWAYS);
         topBar.setAlignment(Pos.CENTER_LEFT);
         topBar.getChildren().addAll(settings, restart);
-//        topBar.getChildren().forEach(x->{
-//            if(x.getClass() == Label.class) {
-//                Label lb = (Label) x;
-//                lb.setFont(Font.font(15));
-//            }
-//        });
-//        settings.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         root.setTop(topBar);
 
-
+        //Grid 'checker' visual
         gameGridVisual.setBackground(new Background(new BackgroundFill(Color.BLACK, new CornerRadii(4, false), new Insets(10, 10, 10, 10))));
 
 
@@ -346,22 +279,40 @@ public class HelloApplication extends Application {
         stage.setTitle("XO Game");
         stage.setScene(mainScene);
         stage.show();
+    }
 
-//        //TODO: this is for debugging
-//        System.out.println(gameGridVisual.getWidth());
-//        gameGridVisual.getChildren().forEach(x-> {
-//            if(x.getClass() != Button.class) {
-//                System.out.println("skip");
-//            } else {
-//                Button btn = (Button) x;
-//                System.out.println(btn.getWidth());
-//                btn.setFont(new Font(31));
-//                System.out.println(btn.fontProperty());
-//                System.out.println(btn.widthProperty());
-//            }
-//
-//        });
+    public GridPane configureGrid() {
+        GridPane gp = new GridPane();
+        gp.setHgap(5);
+        gp.setVgap(5);
 
+        gp.setAlignment(Pos.CENTER);
+
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setPercentWidth(33);
+        RowConstraints rc = new RowConstraints();
+        rc.setPercentHeight(33);
+        for(int i = 0; i < 3; i++) {
+
+            //Apply column and row constraints * 3
+            gp.getColumnConstraints().add(cc);
+            gp.getRowConstraints().add(rc);
+
+            //Construct buttons
+            for(int j = 0; j < 3; j++) {
+                Button button = new Button(Game.getEmptySymbol());
+                button.widthProperty().addListener((observableValue, oldValue, newValue) -> button.setFont(new Font(newValue.doubleValue()/3.0)));
+                button.setStyle("-fx-background-color: %s; -fx-background-radius: 0".formatted("WHITESMOKE"));
+
+                GridPane.setConstraints(button, i, j, 1, 1, HPos.CENTER, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
+                button.maxWidthProperty().bind(gp.widthProperty());
+                button.maxHeightProperty().bind(gp.heightProperty());
+                button.setOnAction((x)-> Game.play(new Pair<>(GridPane.getRowIndex(button), GridPane.getColumnIndex(button))));
+                gp.add(button, i, j);
+            }
+        }
+
+        return gp;
     }
 
     public void processSymbols(TextField p1, TextField p2, TextField p3, Label updateText) {
